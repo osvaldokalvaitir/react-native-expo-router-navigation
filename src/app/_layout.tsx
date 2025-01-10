8f9d7b4ebe722b0322da4c2ddbbe1f0b84c1a1e4
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2025-01-09 21:53:20 -0300

    Criar uma rota de layout

---
+++ src/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { View } from 'react-native'
+import { Slot } from 'expo-router'
+
+export default function Layout() {
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ width: '100%', height: 70, backgroundColor: '#000' }} />
+
+      <Slot />
+
+      <View style={{ width: '100%', height: 50, backgroundColor: '#C3C3C3' }} />
+    </View>
+  )
+}
