91cc38270173833701b8a207a0555f08c402c096
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2025-01-11 15:18:40 -0300

    Reorganizando a aplicação

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { router } from 'expo-router'
-import { DrawerToggleButton } from '@react-navigation/drawer'
 
 export default function Index() {
   function signUp() {
@@ -9,10 +8,6 @@ export default function Index() {
   
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <DrawerToggleButton />
-      </View>
-
       <TouchableOpacity style={styles.button} onPress={signUp}>
         <Text style={styles.label}>Criar conta</Text>
       </TouchableOpacity>
@@ -23,9 +18,9 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
     gap: 32,
-    padding: 32,
   },
   label: {
     fontSize: 16,
@@ -38,9 +33,4 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     borderRadius: 10,
   },
-  header: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'flex-end',
-  }
 })
